716d27c47262bfc57acab4475ed6b997b998853f
MajaCar <mayaplavusa@yahoo.com>
2016-09-22 23:40:48 +0200

    Add files via upload

---
+++ Reading words from file and compare them - are anagrams/src/GitHub/MainClass.java
@@ -0,0 +1,12 @@
+package GitHub;
+
+public class MainClass {
+	 public static void main(String[] args){
+			
+			ReadClass object = new ReadClass();		
+			object.openFile();
+			object.readFile();		
+			object.closeFile();
+				 
+	    }	
+}
