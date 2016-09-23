1dc90a9b791760c24c7265bce5853f28bd95598f
MajaCar <mayaplavusa@yahoo.com>
2016-09-23 20:46:44 +0200

    Update Gui.java

--- GUI Radio buttons for bold, italic text/Gui.java
+++ GUI Radio buttons for bold, italic text/Gui.java
@@ -18,6 +18,8 @@ public class Gui extends JFrame{
 		tf = new JTextField("Recenica", 25);
 		add(tf);
 		
+		// Set RadioButtons for plain button(pb), bold button(bb), italic button(itb) and bold and italic button(bit)
+		
 		pb = new JRadioButton("plain", true);
 		bb = new JRadioButton("bold", false);
 		itb = new JRadioButton("italic", false);
@@ -27,11 +29,15 @@ public class Gui extends JFrame{
 		add(itb);
 		add(bit);
 		
+		// Set group of buttons, so only one from them could be pressed
+		
 		group = new ButtonGroup();
 		group.add(pb);
 		group.add(bb);
 		group.add(itb);
 		group.add(bit);
+		
+		// Set fonts that matches buttons - plain, bold, italic and bold and italic
 		
 		pf = new Font("Serif", Font.PLAIN, 14);
 		bf = new Font("Serif", Font.BOLD, 14);
@@ -39,13 +45,11 @@ public class Gui extends JFrame{
 		bif = new Font("Serif", Font.BOLD + Font.ITALIC, 14);
 		tf.setFont(pf);
 		
-		// wait for event to happen
+		// Wait for event to happen
 		 pb.addItemListener(new Klasa(pf));			
 		 bb.addItemListener(new Klasa(bf));
 		 itb.addItemListener(new Klasa(itf));
 		 bit.addItemListener(new Klasa(bif));
-		
-		
 	}
 	
 	private class Klasa implements ItemListener{		
